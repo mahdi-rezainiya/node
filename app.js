be651d3dcd2c1f6c82f682803ec983391da6cd5e
mahdi <mahdi.rezainiya@gmail.com>
2023-12-07 20:16:15 +0330

    new changes

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ app.listen(3000)
 
 app.get('/' , (req , res) => {
     // res.sendFile('./views/index.html' , {root : __dirname})
-    res.render('index')
+    res.render('index' , {title : 'Home'})
 })
 
 app.get('/about' , (req , res) => {
     // res.sendFile('./views/about.html' , {root : __dirname})
-    res.render('about')
+    res.render('about' , {title : 'About'})
 })
 
 app.get('/blogs/create' , (req , res) => {
-    res.render('create')
+    res.render('create' , {title : 'Create a New Blog'})
 })
 
 // redirect
@@ -34,5 +34,5 @@ app.get('/blogs/create' , (req , res) => {
 
 app.use((req , res) => {
     // res.sendFile('./views/404.html' , {root : __dirname})
-    res.status(404).render('404')
+    res.status(404).render('404' , {title : '404'})
 })
