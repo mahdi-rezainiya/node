d2bef537a2e54ee0426f36f3c1484329af5e337a
mahdi <mahdi.rezainiya@gmail.com>
2023-12-08 12:03:47 +0330

    New Changes

--- app.js
+++ app.js
@@ -10,34 +10,39 @@ app.set('view engine' , 'ejs');
 // listen for request
 app.listen(3000)
 
+app.use((req , res , next) => {
+    console.log("New Request was Made");
+    console.log("Host:" , req.hostname);
+    console.log("Path:" , req.path);
+    console.log("Method:" , req.method);
+    next()
+})
+
 app.get('/' , (req , res) => {
     const blogs = [
         {title : 'bahram in journey' , snippet : 'Lorem ipsum dolor sit amet consectetur.'},
         {title : 'how to fix bugs' , snippet : 'Lorem ipsum dolor sit amet consectetur.'},
         {title : 'nasa finds in planet' , snippet : 'Lorem ipsum dolor sit amet consectetur.'}
     ]
-    // res.sendFile('./views/index.html' , {root : __dirname})
+
     res.render('index' , {title : 'Home' , blogs : blogs})
 })
 
 app.get('/about' , (req , res) => {
-    // res.sendFile('./views/about.html' , {root : __dirname})
     res.render('about' , {title : 'About'})
+})
+
+app.use((req , res , next) => {
+    console.log("---------------------- opencode ---------------------");
+    next()
 })
 
 app.get('/blogs/create' , (req , res) => {
     res.render('create' , {title : 'Create a New Blog'})
 })
 
-// redirect
-
-// app.get('/about-us' , (req , res) => {
-//     res.redirect('/about')
-// })
-
 //404 page
 
 app.use((req , res) => {
-    // res.sendFile('./views/404.html' , {root : __dirname})
     res.status(404).render('404' , {title : '404'})
 })
