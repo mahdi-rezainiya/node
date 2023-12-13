76f5fa5d44be60e5eb8e68ab32deca269eea18d1
mahdi <mahdi.rezainiya@gmail.com>
2023-12-13 22:13:33 +0330

    new changes

--- app.js
+++ app.js
@@ -30,69 +30,99 @@ mongoose.connect(dbURL)
 
 app.use(express.static('public'))
 
+app.use(express.urlencoded({extended : true}))
+
 app.use(morgan('tiny'))
 
 // adding blog
-app.get('/add-blog' , (req , res) => {
-    const blog = new Blog({
-        title : 'new blog 3',
-        snippet : 'about my new blog 3',
-        body :  'Lorem ipsum dolor sit amet consectetur adipisicing.'
-    }) ;
+// app.get('/add-blog' , (req , res) => {
+//     const blog = new Blog({
+//         title : 'new blog 2',
+//         snippet : 'about my new blog 2',
+//         body :  'Lorem ipsum dolor sit amet consectetur adipisicing.'
+//     }) ;
 
-    blog.save()
-        .then((result) => {res.send(result)})
-        .catch((err) => console.log(err))
-})
+//     blog.save()
+//         .then((result) => {res.send(result)})
+//         .catch((err) => console.log(err))
+// })
 
 // getting all blogs
-app.get('/all-blog' , (req , res) => {
-    Blog.find()
-        .then((result) => res.send(result))
-        .catch((err) => {console.log(err);})
-})
+// app.get('/all-blog' , (req , res) => {
+//     Blog.find()
+//         .then((result) => res.send(result))
+//         .catch((err) => {console.log(err);})
+// })
 
 // getting single blog
-app.get('/single-blog' , (req , res) => {
-    Blog.findById('6576b5c8cfa405bdec2839d1')
-        .then((result) => res.send(result))
-        .catch((err) => {console.log(err);})
-})
+// app.get('/single-blog' , (req , res) => {
+//     Blog.findById('6576b5c8cfa405bdec2839d1')
+//         .then((result) => res.send(result))
+//         .catch((err) => {console.log(err);})
+// })
 
 // deleting single blog
-app.get('/d-single-blog' , (req , res) => {
-    Blog.findByIdAndDelete('6576b9d35068b97ee154238f')
-        .then((result) =>{
-            if(result){
-                res.send(`Document deleted : ${result}`)
-            }
-            else{
-                res.send(`Document not found`)
-            }
-        })
-        .catch((err) => {console.log(err);})
-})
+// app.get('/d-single-blog' , (req , res) => {
+//     Blog.findByIdAndDelete('6576b9d35068b97ee154238f')
+//         .then((result) =>{
+//             if(result){
+//                 res.send(`Document deleted : ${result}`)
+//             }
+//             else{
+//                 res.send(`Document not found`)
+//             }
+//         })
+//         .catch((err) => {console.log(err);})
+// })
 
 // deleting all blogs
-app.get('/d-all-blog' , (req , res) => {
-    Blog.deleteMany({})
-        .then((result) => res.send(result))
-        .catch((err) => {console.log(err);})
-})
-
-
+// app.get('/d-all-blog' , (req , res) => {
+//     Blog.deleteMany({})
+//         .then((result) => res.send(result))
+//         .catch((err) => {console.log(err);})
+// })
 
 
 
 app.get('/' , (req , res) => {
-    const blogs = [
-        {title : 'bahram in journey' , snippet : 'Lorem ipsum dolor sit amet consectetur.'},
-        {title : 'how to fix bugs' , snippet : 'Lorem ipsum dolor sit amet consectetur.'},
-        {title : 'nasa finds in planet' , snippet : 'Lorem ipsum dolor sit amet consectetur.'}
-    ]
+    // const blogs = [
+    //     {title : 'bahram in journey' , snippet : 'Lorem ipsum dolor sit amet consectetur.'},
+    //     {title : 'how to fix bugs' , snippet : 'Lorem ipsum dolor sit amet consectetur.'},
+    //     {title : 'nasa finds in planet' , snippet : 'Lorem ipsum dolor sit amet consectetur.'}
+    // ]
+    // res.render('index' , {title : 'Home' , blogs : blogs})
 
-    res.render('index' , {title : 'Home' , blogs : blogs})
+    res.redirect('/blogs')
 })
+
+// blogs routes
+app.get('/blogs' , (req , res) => {
+    Blog.find().sort({createdAt : -1 })
+        .then(blogs => {res.render('index' , {title : 'All Blogs' , blogs})})
+        .catch((err) => {console.log(err)})
+})
+
+app.post('/blogs' , (req , res) => {
+    // console.log(req.body);
+    const blog = new Blog(req.body)
+    
+    blog.save()
+        .then((result) => {res.redirect('/blogs')})
+        .catch((err) => {console.log(err);})
+})
+
+app.get('/blogs/:id' , (req , res) => {
+    const id = req.params.id ;
+
+    // console.log(id); 
+    Blog.findById(id)
+        .then(result => {
+            res.render('details' , {blog : result , title : 'Blog Details'})
+        })
+        .catch(err => console.log(err))
+})
+
+
 
 app.get('/about' , (req , res) => {
     res.render('about' , {title : 'About'})
