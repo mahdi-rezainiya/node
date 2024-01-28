56151fe366f46820971d5bbd34ca3055a1a57f5d
mahdi <mahdi.rezainiya@gmail.com>
2024-01-28 13:25:07 +0330

    new changes

--- model/mysql.js
+++ model/mysql.js
@@ -1,64 +1,43 @@
 const pool = require('../utilities/mysql_database')
 
+// module.exports = CoursesModel;
+
 class CoursesModel {
   static getCourses = async() => {
-    const [result] = await pool.query('select * from employees')
+    const [result] = await pool.query(`select * from person`)
     return result
   }
   
   static getCourse = async(id) => {
-    const [result] = await pool
-      .query(`select * from person 
-                  where id=?` , [id])
+    const [result] = await pool.query(`select * from person
+    where id = ?` , [id])
     return result[0]
   }
+  
   static insertCourse = async (title) => {
-    const [result] = await pool
-      .query(`insert into person(title)
-        values(?)` ,
-        [title]);
-        // return {id : result.insertId , title : title}
-        return getCourse(result.insertId)
+    const result = await pool.query(`insert into person(title)
+    values(?)` , [title])
+    return {id : result.insertId , title : title}
   }
   
-  static updateCourse = async(id , title) => {
+  static updateCourse = async(title , id) => {
     const [result] = await pool.query(`update person
-    set title = ? 
-    where id = ?` ,
+    set title = ?
+    where id = ?`,
     [title , id])
     return getCourse(id)
   }
   
   static deleteCourse = async(id) => {
-    const result = await pool.query(`delete from person
-    where Id = ? ` , [id])
-    // return result
-    return id
+    const [result] = await pool.query(`delete from person
+    where id = ?` , [id])
+    return result
   }
   
   static callStoredProcedure = async(id) => {
     const [result] = await pool.query(`call s_select(?)` , [id])
     return result[0]
   }
-  
 }
 
-module.exports = CoursesModel;
-
-
-
-
-
-// const data = callStoredProcedure(4)
-// .then((result) => console.log(result))
-
-
-
-
-
-
-
-
-
-
-
+module.exports = CoursesModel ;
--- utilities/mysql_database.js
+++ utilities/mysql_database.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql2');
 require('dotenv').config()
 
-
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
